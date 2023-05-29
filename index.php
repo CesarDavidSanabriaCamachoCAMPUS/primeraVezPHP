41b22ed20edfb4090cbb54cacaa67060f7bae449
CesarDavidSanabriaCamachoCAMPUS <cesar.sanabria1307@gmail.com>
2023-05-29 17:04:32 -0500

    feat: :tada: introduccion a php
    se ve los recursos de las carpetas para que sirven y por que son necesarioas adicional a esto se explica para que sirve "echo"

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*  
+    se crean las carpetas correspondientes
+        js          para guardar el java script
+        css         para los estilos en css
+        img         para guardar las iumagenes del proyecto
+        scripts     para guardar todos los archivos del php
+        uploads     es el archivo para generar el autoload
+    tambiuen vemos "echo" sirve para imprimir cadenas de texto
+    en la web puede imprimir mas de una
+*/
+    echo "Texto a imprimir";
+?>

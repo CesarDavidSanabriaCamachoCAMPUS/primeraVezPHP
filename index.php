b7b5b7e09119d96c01aa1ea7efdd8f72af7779af
CesarDavidSanabriaCamachoCAMPUS <cesar.sanabria1307@gmail.com>
2023-06-05 17:26:37 -0500

    feat: :construction: documentacion php
    documentacion de php transcrita como lo entendemos

--- index.php
+++ index.php
@@ -53,6 +53,57 @@
     // declara variable de booleano
     define ("esValido", true);
 
+// punto 8
+/*
+    existen tipos de datos pára almacenar tipos de informacion en php
+        (int)       almacena nuemeros enteros sin decimales
+        (float)     almacena numeros con decimales}
+        (string)    almacena texto o caracteres (string)
+        (bool)      almacena valores true o false (verdadero o falso)
+        (array)     almacena colecciones de datos que puedes ser diferentes
+        (object)    almacena instancias de clases,que son definiciones de objetos
+        (resourse)  almacena recursos de bases de datos o archivos abiertos (recursos externos)
+        (null)      representa variables sin valor o sin definir (no contiene nada o no se ha declarado)
+*/
+    // boolean
+    $boolean=true;
+    var_dump($boolean);
+    // enteros
+    $numero=13;
+    var_dump($numero);
+    // float
+    $float=13.5;
+    var_dump($float);
+    //string
+    $string="Cesar";
+    var_dump($string);
+    // array
+    $array=["Cesar", 13, 13.5, true];
+    var_dump($array);
+    
+// punto 9
+/*
+    los siguientes son los operadores que se usan en php:
+            aritmeticos             asignacion                array                           bitwise
 
+    (+)     suma              (=)     igual que         (+)     union               (&)     y (and)
+    (-)     resta                                       (==)    igualdad            (^)     verdadero solo uno
+    (*)     multiplicacion                              (===)   identico            (|)     no
+    (/)     division                                    (!=)    desigual            (<<)    shift izq
+    (%)     modulo                                      (<>)    desigual            (>>)    shift der
+    (*)     potencia                                    (!==)   no es indentico 
+     
+            comparacion                                         ejecucion                      incremento/reduccion
 
+    (==)        igualdad                           (``)    para poder dar espacio           (++$a)      pre-incremento
+    (===)       identico                                                                    ($a++)      post-incremento
+    (!==)       no identico                                     control de error            (--$a)      pre-reduccion
+    (<>)        desigual                                                                    ($a--)      post-reduccion
+    (<)         menor que                          (@)     ignora los mensajes de error
+    (<=)        menor o igual que
+    (>)         mayor que                           
+    (>=)        mayor o igual que
+    (<=>)       adicion izq num (-) der num (+)
+
+*/
 ?>
